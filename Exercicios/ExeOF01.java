210c366208475a1749b3d9a0e5bbb4095bbab9b1
Calebe Evangelista <calebeevangelista@Calebe-Kali.Calebe-Corp>
2020-11-28 19:53:05 -0300

    Adicionei o ExeOF01 a pasta de exercicios

--- Exercicios/ExeOF01.java
+++ Exercicios/ExeOF01.java
@@ -1,12 +1,21 @@
 package Exercicios;
-
+import java.util.Scanner;
 public class ExeOF01 {
 
 	public static void main (String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 //Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa, conforme exemplos.
 		
+		int v1,v2,soma;
 		
+		v1 = sc.nextInt();
+		v2 = sc.nextInt();
+		soma = v1 + v2;
 		
+		System.out.println("SOMA = " + soma);
+				
+		sc.close(); 		
 	}
 }

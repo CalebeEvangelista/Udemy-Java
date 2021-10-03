eec30cc1cb50de3622b1a4ae34b74d59036e41c1
Calebe Evangelista <calebe.leao@hotmail.com>
2021-10-03 12:36:56 -0300

    Adicionando os codigos da aula 71

---
+++ Aula71/Program.java
@@ -0,0 +1,22 @@
+package Aula71;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entre com o raio: ");
+        double raio = sc.nextDouble();
+
+        double c = Calculator.circumferencia(raio);
+        double v = Calculator.volume(raio);
+
+        System.out.printf("Circuferencia: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.printf("Valor de PI: %.2f%n", Calculator.PI);
+
+        sc.close();
+    }
+}
+
